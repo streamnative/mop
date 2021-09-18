f390a8dc217f4318a5ab472bef1056127c89a7b2
technoboy <technoboy@apache.org>
2021-09-18 21:45:02 +0800

    throw ioexception

--- mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/support/psk/PSKUtils.java
+++ mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/support/psk/PSKUtils.java
@@ -17,6 +17,7 @@ import com.google.common.base.Splitter;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.List;
@@ -30,7 +31,7 @@ public class PSKUtils {
             while ((line = reader.readLine()) != null) {
                 result.addAll(parse(line));
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
         return result;
