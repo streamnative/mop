348324e006811125f9b7402ffd24b78de342464e
technoboy <technoboy@apache.org>
2021-10-20 21:07:53 +0800

    Refactor test part and add test for authorization.

--- tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/base/BasicAuthenticationConfig.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/base/BasicAuthenticationConfig.java
@@ -11,13 +11,13 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.streamnative.pulsar.handlers.mqtt;
+package io.streamnative.pulsar.handlers.mqtt.base;
 
 import static org.mockito.Mockito.spy;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 import com.google.common.collect.Sets;
-import io.streamnative.pulsar.handlers.mqtt.base.MQTTTestBase;
+import io.streamnative.pulsar.handlers.mqtt.MQTTServerConfiguration;
 import java.net.URISyntaxException;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
@@ -25,20 +25,13 @@ import org.apache.pulsar.broker.authentication.AuthenticationProviderBasic;
 import org.apache.pulsar.client.admin.PulsarAdmin;
 import org.apache.pulsar.client.api.PulsarClient;
 import org.apache.pulsar.client.impl.auth.AuthenticationBasic;
-import org.fusesource.mqtt.client.BlockingConnection;
 import org.fusesource.mqtt.client.MQTT;
-import org.fusesource.mqtt.client.MQTTException;
-import org.fusesource.mqtt.client.Message;
-import org.fusesource.mqtt.client.QoS;
-import org.fusesource.mqtt.client.Topic;
-import org.testng.Assert;
-import org.testng.annotations.Test;
 
 /**
- * Basic authentication integration tests for MQTT protocol handler.
+ * Basic authentication config.
  */
 @Slf4j
-public class BasicAuthenticationIntegrationTest extends MQTTTestBase {
+public class BasicAuthenticationConfig extends MQTTTestBase {
 
     @Override
     public MQTTServerConfiguration initConfig() throws Exception{
@@ -46,7 +39,6 @@ public class BasicAuthenticationIntegrationTest extends MQTTTestBase {
         System.setProperty("pulsar.auth.basic.conf", "./src/test/resources/htpasswd");
         String authParams = "{\"userId\":\"superUser\",\"password\":\"supepass\"}";
 
-        conf.setMqttProxyEnabled(true);
         conf.setAuthenticationEnabled(true);
         conf.setMqttAuthenticationEnabled(true);
         conf.setMqttAuthenticationMethods(ImmutableList.of("basic"));
@@ -80,41 +72,5 @@ public class BasicAuthenticationIntegrationTest extends MQTTTestBase {
         mqtt.setUserName("superUser");
         mqtt.setPassword("supepass");
         return mqtt;
-    }
-
-    @Test(timeOut = TIMEOUT)
-    public void testAuthenticateAndPublish() throws Exception {
-        MQTT mqtt = createMQTTClient();
-        authenticateAndPublish(mqtt);
-    }
-
-    @Test(timeOut = TIMEOUT)
-    public void testAuthenticateAndPublishViaProxy() throws Exception {
-        MQTT mqtt = createMQTTProxyClient();
-        authenticateAndPublish(mqtt);
-    }
-
-    public void authenticateAndPublish(MQTT mqtt) throws Exception {
-        String topicName = "persistent://public/default/testAuthentication";
-        BlockingConnection connection = mqtt.blockingConnection();
-        connection.connect();
-        Topic[] topics = {new Topic(topicName, QoS.AT_LEAST_ONCE)};
-        connection.subscribe(topics);
-        String message = "Hello MQTT";
-        connection.publish(topicName, message.getBytes(), QoS.AT_LEAST_ONCE, false);
-        Message received = connection.receive();
-        Assert.assertEquals(received.getTopic(), topicName);
-        Assert.assertEquals(new String(received.getPayload()), message);
-        received.ack();
-        connection.disconnect();
-    }
-
-    @Test(expectedExceptions = {MQTTException.class}, timeOut = TIMEOUT)
-    public void testInvalidCredentials() throws Exception {
-        MQTT mqtt = createMQTTClient();
-        mqtt.setPassword("invalid");
-        BlockingConnection connection = mqtt.blockingConnection();
-        connection.connect();
-        connection.disconnect();
     }
 }
