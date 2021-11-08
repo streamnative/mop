7e8a67ab68d1eb1cd4120eda3a0110e260a77fe0
technoboy <technoboy@apache.org>
2021-11-08 10:04:31 +0800

    Support for last will message semantic.

--- mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/utils/NettyUtils.java
+++ mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/utils/NettyUtils.java
@@ -20,6 +20,7 @@ import static io.streamnative.pulsar.handlers.mqtt.Constants.ATTR_CONNECTION;
 import static io.streamnative.pulsar.handlers.mqtt.Constants.ATTR_CONNECT_MSG;
 import static io.streamnative.pulsar.handlers.mqtt.Constants.ATTR_KEEP_ALIVE_TIME;
 import static io.streamnative.pulsar.handlers.mqtt.Constants.ATTR_TOPIC_SUBS;
+import static io.streamnative.pulsar.handlers.mqtt.Constants.ATTR_WILL_MESSAGE;
 import io.netty.channel.Channel;
 import io.netty.channel.ChannelPipeline;
 import io.netty.handler.codec.mqtt.MqttConnectMessage;
@@ -51,6 +52,7 @@ public final class NettyUtils {
     private static final AttributeKey<Object> ATTR_KEY_CONNECT_MSG = AttributeKey.valueOf(ATTR_CONNECT_MSG);
     private static final AttributeKey<Object> ATTR_KEY_TOPIC_SUBS = AttributeKey.valueOf(ATTR_TOPIC_SUBS);
     private static final AttributeKey<Object> ATTR_KEY_CLIENT_ADDR = AttributeKey.valueOf(ATTR_CLIENT_ADDR);
+    private static final AttributeKey<Object> ATTR_KEY_WILL_MESSAGE = AttributeKey.valueOf(ATTR_WILL_MESSAGE);
 
     public static void setClientId(Channel channel, String clientId) {
         channel.attr(NettyUtils.ATTR_KEY_CLIENT_ID).set(clientId);
@@ -142,6 +144,15 @@ public final class NettyUtils {
 
     public static String getAddress(Channel channel) {
         return (String) channel.attr(NettyUtils.ATTR_KEY_CLIENT_ADDR).get();
+    }
+
+    public static void setWillMessage(Channel channel, WillMessage willMessage) {
+        channel.attr(NettyUtils.ATTR_KEY_WILL_MESSAGE).set(willMessage);
+    }
+
+    public static Optional<WillMessage> getWillMessage(Channel channel) {
+        WillMessage willMessage = (WillMessage) channel.attr(NettyUtils.ATTR_KEY_WILL_MESSAGE).get();
+        return Optional.ofNullable(willMessage);
     }
 
     private NettyUtils() {
