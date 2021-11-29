29a8d2ee03c4efd632ad55494e653b370b65b995
Qiang Zhao <mattisonchao@gmail.com>
2021-11-29 15:31:38 +0800

    to pass checkstyle

--- tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/CleanSessionTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/CleanSessionTest.java
@@ -17,10 +17,11 @@ import com.hivemq.client.mqtt.mqtt3.Mqtt3BlockingClient;
 import com.hivemq.client.mqtt.mqtt3.Mqtt3Client;
 import com.hivemq.client.mqtt.mqtt3.message.connect.connack.Mqtt3ConnAck;
 import io.streamnative.pulsar.handlers.mqtt.base.MQTTTestBase;
+import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import java.util.UUID;
+
 
 @Slf4j
 public class CleanSessionTest extends MQTTTestBase {
