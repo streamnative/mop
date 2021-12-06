9af72434bf62426a69b7117fe00e1c5f98ae9188
Qiang Zhao <74767115+mattisonchao@users.noreply.github.com>
2021-12-06 10:24:04 +0800

    Support disconnect session expire interval (#299)
    * Support disconnect session expire interval
    
    This feature detail in MQTT5 spec 3.14.2.2.2
    
    - Support disconnect contains session expire interval time to update connect sent value.
    - Support disconnect session expire interval value check.
    - Support session dose not expire when value is 0xFFFFFFFF (UINT_MAX)

--- tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/mqtt5/hivemq/base/MQTT5SessionExpireIntervalTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/mqtt5/hivemq/base/MQTT5SessionExpireIntervalTest.java
@@ -15,7 +15,9 @@ package io.streamnative.pulsar.handlers.mqtt.mqtt5.hivemq.base;
 
 import com.hivemq.client.mqtt.datatypes.MqttQos;
 import com.hivemq.client.mqtt.mqtt5.Mqtt5BlockingClient;
+import com.hivemq.client.mqtt.mqtt5.exceptions.Mqtt5DisconnectException;
 import com.hivemq.client.mqtt.mqtt5.message.connect.connack.Mqtt5ConnAck;
+import com.hivemq.client.mqtt.mqtt5.message.disconnect.Mqtt5DisconnectReasonCode;
 import io.streamnative.pulsar.handlers.mqtt.base.MQTTTestBase;
 import io.streamnative.pulsar.handlers.mqtt.utils.PulsarTopicUtils;
 import java.util.List;
@@ -103,5 +105,80 @@ public class MQTT5SessionExpireIntervalTest extends MQTTTestBase {
                 });
     }
 
+    @Test(timeOut = TIMEOUT)
+    public void testDisconnectSendNewExpireInterval() throws Exception {
+        final String topic = "test-expire-interval-3";
+        Mqtt5BlockingClient client = MQTT5ClientUtils.createMqtt5Client(getMqttBrokerPortList().get(0));
+        client.connectWith()
+                .cleanStart(false)
+                .sessionExpiryInterval(5)
+                .send();
+        client.subscribeWith()
+                .topicFilter(topic)
+                .qos(MqttQos.AT_LEAST_ONCE)
+                .send();
+        String pulsarTopicName = PulsarTopicUtils
+                .getPulsarTopicName(topic, defaultTenant, defaultNamespace, true, defaultTopicDomain);
+        String clientId = Objects.requireNonNull(client.getConfig().getClientIdentifier().orElse(null)).toString();
+        client.disconnectWith()
+                .sessionExpiryInterval(10)
+                .send();
+        Awaitility.await()
+                .pollDelay(8, TimeUnit.SECONDS)
+                .untilAsserted(() -> {
+                    List<String> reconnectSubscriptions = admin.topics().getSubscriptions(pulsarTopicName);
+                    Assert.assertTrue(reconnectSubscriptions.contains(clientId));
+                });
+        Awaitility.await()
+                .untilAsserted(()->{
+                    List<String> reconnectSubscriptions = admin.topics().getSubscriptions(pulsarTopicName);
+                    Assert.assertFalse(reconnectSubscriptions.contains(clientId));
+                });
+    }
+
+
+    @Test(timeOut = TIMEOUT)
+    public void testDisconnectSend0IntervalToCloseSession() throws Exception {
+        final String topic = "test-expire-interval-4";
+        Mqtt5BlockingClient client = MQTT5ClientUtils.createMqtt5Client(getMqttBrokerPortList().get(0));
+        client.connectWith()
+                .cleanStart(false)
+                .sessionExpiryInterval(5)
+                .send();
+        client.subscribeWith()
+                .topicFilter(topic)
+                .qos(MqttQos.AT_LEAST_ONCE)
+                .send();
+        String pulsarTopicName = PulsarTopicUtils
+                .getPulsarTopicName(topic, defaultTenant, defaultNamespace, true, defaultTopicDomain);
+        String clientId = Objects.requireNonNull(client.getConfig().getClientIdentifier().orElse(null)).toString();
+        client.disconnectWith()
+                .sessionExpiryInterval(0)
+                .send();
+        Awaitility.await()
+                .atMost(2, TimeUnit.SECONDS)
+                .untilAsserted(()->{
+                    List<String> reconnectSubscriptions = admin.topics().getSubscriptions(pulsarTopicName);
+                    Assert.assertFalse(reconnectSubscriptions.contains(clientId));
+                });
+    }
+
+
+    @Test(timeOut = TIMEOUT)
+    public void testDisconnectGotProtocolError() throws Exception {
+        Mqtt5BlockingClient client = MQTT5ClientUtils.createMqtt5Client(getMqttBrokerPortList().get(0));
+        client.connectWith()
+                .cleanStart(false)
+                .sessionExpiryInterval(0)
+                .send();
+        try {
+            client.disconnectWith()
+                    .sessionExpiryInterval(10)
+                    .send();
+        } catch (Mqtt5DisconnectException ex){
+            Mqtt5DisconnectReasonCode reasonCode = ex.getMqttMessage().getReasonCode();
+            Assert.assertEquals(reasonCode, Mqtt5DisconnectReasonCode.PROTOCOL_ERROR);
+        }
+    }
 
 }
