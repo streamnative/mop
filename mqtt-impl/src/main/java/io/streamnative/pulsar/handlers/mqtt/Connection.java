c979d0adac8defd617657a5a31002666c9a150ea
Qiang Zhao <mattisonchao@gmail.com>
2021-12-16 17:06:42 +0800

    fix

--- mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/Connection.java
+++ mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/Connection.java
@@ -36,7 +36,6 @@ import java.util.Objects;
 import java.util.concurrent.atomic.AtomicIntegerFieldUpdater;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import lombok.Getter;
-import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.pulsar.broker.service.BrokerServiceException;
