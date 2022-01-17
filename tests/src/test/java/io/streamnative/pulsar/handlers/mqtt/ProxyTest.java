9bde056daacc328c90d54167861448e48c83b14c
technoboy <technoboy@apache.org>
2022-01-17 22:27:10 +0800

    fix checkstyle

--- tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/ProxyTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/ProxyTest.java
@@ -24,7 +24,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
 
 /**
  * MQTT proxy related test.
