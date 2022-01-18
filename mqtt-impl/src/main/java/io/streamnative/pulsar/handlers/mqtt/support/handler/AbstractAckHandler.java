5a6dfc1774df342fd2cf38b2aed24e940bb5e258
Qiang Zhao <74767115+mattisonchao@users.noreply.github.com>
2022-01-18 21:03:18 +0800

    [ MIP 1 ] Support publish acknowledgement (#406)

--- mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/support/handler/AbstractAckHandler.java
+++ mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/support/handler/AbstractAckHandler.java
@@ -21,9 +21,12 @@ import io.netty.channel.ChannelFuture;
 import io.netty.handler.codec.mqtt.MqttMessage;
 import io.streamnative.pulsar.handlers.mqtt.Connection;
 import io.streamnative.pulsar.handlers.mqtt.messages.ack.DisconnectAck;
+import io.streamnative.pulsar.handlers.mqtt.messages.ack.PublishAck;
 import io.streamnative.pulsar.handlers.mqtt.messages.ack.SubscribeAck;
 import io.streamnative.pulsar.handlers.mqtt.messages.factory.MqttConnectAckHelper;
 import io.streamnative.pulsar.handlers.mqtt.messages.factory.MqttDisconnectAckMessageHelper;
+import io.streamnative.pulsar.handlers.mqtt.messages.factory.MqttPubAckMessageHelper;
+import io.streamnative.pulsar.handlers.mqtt.utils.MqttUtils;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -35,6 +38,8 @@ public abstract class AbstractAckHandler implements AckHandler {
     abstract MqttMessage getConnAckMessage(Connection connection);
 
     abstract MqttMessage getSubscribeAckMessage(Connection connection, SubscribeAck subscribeAck);
+
+    abstract MqttMessage getPublishAckMessage(Connection connection, PublishAck publishAck);
 
     abstract MqttMessage getDisconnectAckMessage(Connection connection, DisconnectAck disconnectAck);
 
@@ -92,6 +97,27 @@ public abstract class AbstractAckHandler implements AckHandler {
                             .reasonString(disconnectAck.getReasonStr())
                             .build();
             return connection.sendThenClose(disconnectErrorAck);
+        }
+    }
+
+    @Override
+    public ChannelFuture sendPublishAck(Connection connection, PublishAck publishAck) {
+        if (publishAck.isSuccess()){
+            MqttMessage publishAckMessage = getPublishAckMessage(connection, publishAck);
+            return connection.send(publishAckMessage);
+        } else {
+            if (MqttUtils.isMqtt5(connection.getProtocolVersion())) {
+                MqttMessage pubErrorAck = MqttPubAckMessageHelper
+                        .errorBuilder(connection.getProtocolVersion())
+                        .packetId(publishAck.getPacketId())
+                        .errorReason(publishAck.getErrorReason())
+                        .reasonString(publishAck.getReasonString())
+                        .build();
+                return connection.sendThenClose(pubErrorAck);
+            } else {
+                // mqtt 3.x do not have any ack.
+                return connection.getChannel().close();
+            }
         }
     }
 }
