475bf31336f4f6e0ced1314bb72af23b84399a59
mattison chao <mattisonchao@gmail.com>
2022-03-26 21:49:23 +0800

    Fix test

--- tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/proxy/EventCenterWithProxyTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/proxy/EventCenterWithProxyTest.java
@@ -11,7 +11,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.streamnative.pulsar.handlers.mqtt.mqtt5.hivemq.proxy;
+package io.streamnative.pulsar.handlers.mqtt.proxy;
 
 import io.streamnative.pulsar.handlers.mqtt.MQTTProtocolHandler;
 import io.streamnative.pulsar.handlers.mqtt.MQTTServerConfiguration;
