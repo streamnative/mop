2b88c8d53228d3ea9e684d8b32512ac0c6263764
Jiwe Guo <technoboy@apache.org>
2022-06-14 17:15:52 +0800

    apply comment.

--- mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/utils/PulsarMessageConverter.java
+++ mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/utils/PulsarMessageConverter.java
@@ -131,6 +131,7 @@ public class PulsarMessageConverter {
                             break;
                         default:
                             log.warn("invalid propertyType: {}", propertyType);
+                            break;
                     }
                 }
             }
