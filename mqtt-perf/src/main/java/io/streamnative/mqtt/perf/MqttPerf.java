c9719c99f8b4c9c8bd932b0390e2433804e8561b
mattisonchao <mattisonchao@apache.org>
2023-07-29 11:58:46 +0800

    Add banner

--- mqtt-perf/src/main/java/io/streamnative/mqtt/perf/MqttPerf.java
+++ mqtt-perf/src/main/java/io/streamnative/mqtt/perf/MqttPerf.java
@@ -4,12 +4,15 @@ import static java.lang.System.exit;
 
 import picocli.CommandLine;
 
+import java.nio.file.Files;
 import java.text.DecimalFormat;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.logging.Logger;
 
 public final class MqttPerf {
+    private static final Logger LOG = Logger.getLogger("Banner");
     public static final int MQTT_VERSION_3 = 3;
     public static final int MQTT_VERSION_5 = 5;
     public static final ScheduledExecutorService EXECUTOR = Executors.newScheduledThreadPool(
@@ -21,6 +24,17 @@ public final class MqttPerf {
 
     public static void main(String[] args) {
         System.setProperty("java.util.logging.SimpleFormatter.format", "[%1$tF %1$tT] [%4$-7s] %5$s %n");
+        printBanner();
         exit(new CommandLine(new CommandPerf()).execute(args));
+    }
+
+    public static void printBanner() {
+        final var banner = "\n" +
+                ",--.   ,--. ,-----.,--------.,--------. ,------.               ,---. \n" +
+                "|   `.'   |'  .-.  '--.  .--''--.  .--' |  .--. ',---. ,--.--./  .-' \n" +
+                "|  |'.'|  ||  | |  |  |  |      |  |    |  '--' | .-. :|  .--'|  `-, \n" +
+                "|  |   |  |'  '-'  '-.|  |      |  |    |  | --'\\   --.|  |   |  .-' \n" +
+                "`--'   `--' `-----'--'`--'      `--'    `--'     `----'`--'   `--'   \n";
+        LOG.info(banner);
     }
 }
