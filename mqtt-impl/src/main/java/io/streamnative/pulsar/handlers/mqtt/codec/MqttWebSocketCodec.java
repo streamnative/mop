377c02c054ac55084d2d8f7ad8747640c967889c
caihualin <caihualin@apusic.com>
2024-04-17 17:46:29 +0800

    MoP accept ws connections

---
+++ mqtt-impl/src/main/java/io/streamnative/pulsar/handlers/mqtt/codec/MqttWebSocketCodec.java
@@ -0,0 +1,24 @@
+package io.streamnative.pulsar.handlers.mqtt.codec;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.MessageToMessageCodec;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import java.util.List;
+
+/**
+ * WebSocket Mqtt message codec
+ *
+ */
+public class MqttWebSocketCodec extends MessageToMessageCodec<BinaryWebSocketFrame, ByteBuf> {
+
+	@Override
+	protected void encode(ChannelHandlerContext ctx, ByteBuf msg, List<Object> out) throws Exception {
+		out.add(new BinaryWebSocketFrame(msg.retain()));
+	}
+
+	@Override
+	protected void decode(ChannelHandlerContext ctx, BinaryWebSocketFrame msg, List<Object> out) throws Exception {
+		out.add(msg.retain().content());
+	}
+}
