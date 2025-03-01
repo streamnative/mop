9ba7541ebe550e969a887dfe08520f662ea509d9
Jiwe Guo <technoboy@apache.org>
2025-03-01 15:49:28 +0800

    Fix proxy connect to multi broker issue

--- mqtt-proxy/src/main/java/io/streamnative/pulsar/handlers/mqtt/proxy/channel/MQTTProxyAdapter.java
+++ mqtt-proxy/src/main/java/io/streamnative/pulsar/handlers/mqtt/proxy/channel/MQTTProxyAdapter.java
@@ -45,10 +45,8 @@ import io.streamnative.pulsar.handlers.mqtt.common.utils.MqttUtils;
 import io.streamnative.pulsar.handlers.mqtt.proxy.MQTTProxyService;
 import io.streamnative.pulsar.handlers.mqtt.proxy.impl.MQTTProxyProtocolMethodProcessor;
 import java.net.InetSocketAddress;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentMap;
@@ -157,17 +155,6 @@ public class MQTTProxyAdapter {
 
         public static final String NAME = "adapter-handler";
 
-        private final Set<Connection> callbackConnections = Collections.newSetFromMap(new ConcurrentHashMap<>());
-
-        public void registerAdapterChannelInactiveListener(Connection connection) {
-            callbackConnections.add(connection);
-        }
-
-        @Override
-        public void channelInactive(ChannelHandlerContext ctx) throws Exception {
-            callbackConnections.forEach(connection -> connection.getChannel().close());
-        }
-
         @Override
         public void channelRead(ChannelHandlerContext ctx, Object message) throws Exception {
             checkArgument(message instanceof MqttAdapterMessage);
@@ -191,6 +178,7 @@ public class MQTTProxyAdapter {
                 }
                 switch (messageType) {
                     case DISCONNECT:
+                        log.info("Received DISCONNECT from adapter, closing connection {}", connection);
                         if (MqttUtils.isNotMqtt3(connection.getProtocolVersion())) {
                             clientChannel.writeAndFlush(adapterMsg);
                         }
---
+++ tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/mqtt3/paho/proxy/TestProxyConnectMultiBroker.java
@@ -0,0 +1,97 @@
+package io.streamnative.pulsar.handlers.mqtt.mqtt3.paho.proxy;
+
+import io.streamnative.pulsar.handlers.mqtt.base.MQTTTestBase;
+import io.streamnative.pulsar.handlers.mqtt.common.MQTTCommonConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
+import org.eclipse.paho.client.mqttv3.IMqttMessageListener;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttCallback;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Test
+@Slf4j
+public class TestProxyConnectMultiBroker extends MQTTTestBase {
+
+    @Override
+    protected MQTTCommonConfiguration initConfig() throws Exception {
+        MQTTCommonConfiguration mqtt = super.initConfig();
+        mqtt.setDefaultNumberOfNamespaceBundles(4);
+        mqtt.setMqttProxyEnabled(true);
+        return mqtt;
+    }
+
+    public static class Callback implements MqttCallback {
+
+        @Override
+        public void connectionLost( java.lang.Throwable throwable ) {
+            log.info("Connection lost");
+        }
+
+        @Override
+        public void messageArrived( java.lang.String s, MqttMessage mqttMessage ) throws Exception {
+            log.info("Message arrived");
+        }
+
+        @Override
+        public void deliveryComplete( IMqttDeliveryToken iMqttDeliveryToken ) {
+        }
+    }
+
+    @Test(timeOut = 1000 * 60 * 5)
+    public void testProxyConnectMultiBroker() throws Exception {
+        int port = getMqttProxyPortList().get(0);
+        MqttAsyncClient client = new MqttAsyncClient(  "tcp://localhost:" + port, "test", new MemoryPersistence() );
+        MqttConnectOptions connectOptions = new MqttConnectOptions();
+        connectOptions.setCleanSession( true );
+        connectOptions.setKeepAliveInterval(5);
+        log.info("connecting...");
+        client.connect( connectOptions ).waitForCompletion();
+        log.info("connected");
+
+        client.subscribe( "testsub1", 1 ).waitForCompletion();
+        log.info("subscribed testsub1");
+        // sleep the keep alive period to show that PING will happen in abscence of other messages.
+        Thread.sleep( 6000 );
+
+        // make more subscriptions to connect to multiple brokers.
+        client.subscribe( "testsub2", 1 ).waitForCompletion();
+        log.info("subscribed testsub2");
+        client.subscribe( "testsub3", 1 ).waitForCompletion();
+        log.info("subscribed testsub3");
+        Map<String, List<String>> msgs = new HashMap<>();
+        String topic = "test1";
+        client.subscribe( topic, 1, new IMqttMessageListener() {
+
+            @Override
+            public void messageArrived(String topic, MqttMessage message) throws Exception {
+                msgs.compute(topic, (k, v) -> {
+                    if (v == null) {
+                        v = new ArrayList<>();
+                    }
+                    v.add(new String(message.getPayload()));
+                    return v;
+                });
+            }
+        }).waitForCompletion();
+
+        // publish QoS 1 message to prevent the need for PINGREQ. Keep alive only sends ping in abscence of other
+        // messages. Refer to section 3.1.2.10 of the MQTT 3.1.1 specification.
+        for(int i = 0; i < 130; i ++) {
+            log.info("Publishing message..." + System.currentTimeMillis());
+            client.publish( topic, "test".getBytes(), 1, false ).waitForCompletion();
+            Thread.sleep( 1000 );
+        }
+        Assert.assertNotNull(msgs.get(topic) != null);
+        Assert.assertEquals(msgs.get(topic).size(), 130);
+        client.disconnect().waitForCompletion();
+    }
+}
