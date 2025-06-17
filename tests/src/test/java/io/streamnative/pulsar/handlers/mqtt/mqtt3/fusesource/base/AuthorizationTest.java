ccc1e63844052b6c995331203fb0db21f698ecf8
Cong Zhao <zhaocong@apache.org>
2025-06-17 17:31:02 +0800

    Fix subscription authorization PREFIX mode (#1756)

--- tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/mqtt3/fusesource/base/AuthorizationTest.java
+++ tests/src/test/java/io/streamnative/pulsar/handlers/mqtt/mqtt3/fusesource/base/AuthorizationTest.java
@@ -21,6 +21,7 @@ import java.util.Set;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.pulsar.client.admin.GrantTopicPermissionOptions;
 import org.apache.pulsar.common.policies.data.AuthAction;
+import org.apache.pulsar.common.policies.data.SubscriptionAuthMode;
 import org.awaitility.Awaitility;
 import org.fusesource.mqtt.client.BlockingConnection;
 import org.fusesource.mqtt.client.MQTT;
@@ -111,6 +112,58 @@ public class AuthorizationTest extends AuthorizationConfig {
         Assert.assertEquals(new String(receive.getPayload()), message);
         producer.disconnect();
         consumer.disconnect();
+    }
+
+    @Test(timeOut = TIMEOUT)
+    public void testSubscriptionAuthMode() throws Exception {
+        Set<AuthAction> user1Actions = new HashSet<>();
+        user1Actions.add(AuthAction.produce);
+        admin.namespaces().grantPermissionOnNamespace("public/default", "user1", user1Actions);
+
+        Set<AuthAction> user2Actions = new HashSet<>();
+        user2Actions.add(AuthAction.consume);
+        admin.namespaces().grantPermissionOnNamespace("public/default", "user2", user2Actions);
+
+        admin.namespaces().setSubscriptionAuthMode("public/default", SubscriptionAuthMode.Prefix);
+
+        String topicName = "persistent://public/default/testSubscriptionAuthMode";
+        MQTT mqttConsumer = createMQTTClient();
+        mqttConsumer.setClientId("user2-11");
+        mqttConsumer.setUserName("user2");
+        mqttConsumer.setPassword("pass2");
+        BlockingConnection consumer = mqttConsumer.blockingConnection();
+        consumer.connect();
+        Topic[] topics = {new Topic(topicName, QoS.AT_LEAST_ONCE)};
+        consumer.subscribe(topics);
+
+        MQTT mqttProducer = createMQTTClient();
+        mqttProducer.setUserName("user1");
+        mqttProducer.setPassword("pass1");
+        BlockingConnection producer = mqttProducer.blockingConnection();
+        producer.connect();
+        String message = "Hello MQTT";
+        producer.publish(topicName, message.getBytes(), QoS.AT_MOST_ONCE, false);
+
+        Message receive = consumer.receive();
+        Assert.assertEquals(new String(receive.getPayload()), message);
+        producer.disconnect();
+        consumer.disconnect();
+
+        MQTT mqttConsumer3 = createMQTTClient();
+        mqttConsumer3.setClientId("user3-11");
+        mqttConsumer3.setUserName("user2");
+        mqttConsumer3.setPassword("pass2");
+        mqttConsumer3.setConnectAttemptsMax(0);
+        mqttConsumer3.setReconnectAttemptsMax(0);
+        BlockingConnection connection3 = mqttConsumer3.blockingConnection();
+        connection3.connect();
+        Awaitility.await().untilAsserted(() -> {
+            Assert.assertTrue(connection3.isConnected());
+        });
+        connection3.subscribe(topics);
+        Awaitility.await().untilAsserted(() -> {
+            Assert.assertFalse(connection3.isConnected());
+        });
     }
 
     @Test
