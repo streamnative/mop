a57d06db2a60f971d588cc27a9547aa102066b9a
Jiwe Guo <technoboy@apache.org>
2025-10-21 10:37:38 +0800

    update pulsar and sn dependency version

--- mqtt-common/src/main/java/io/streamnative/pulsar/handlers/mqtt/common/event/PulsarTopicChangeListener.java
+++ mqtt-common/src/main/java/io/streamnative/pulsar/handlers/mqtt/common/event/PulsarTopicChangeListener.java
@@ -15,7 +15,7 @@ package io.streamnative.pulsar.handlers.mqtt.common.event;
 
 import io.streamnative.pulsar.handlers.mqtt.common.utils.EventParserUtils;
 import java.util.regex.Pattern;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.pulsar.common.naming.SystemTopicNames;
 import org.apache.pulsar.common.naming.TopicName;
 
